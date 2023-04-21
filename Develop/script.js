4ffa2dd0885df2873ead2e6cd65bb161075ee2c8
33hollie33 <hollybarnhurst@gmail.com>
2023-04-21 13:43:36 +0100

    Added the prompt within the code and comments

--- Develop/script.js
+++ Develop/script.js
@@ -1,20 +1,48 @@
- // Assignment Code
 var generateBtn = document.querySelector("#generate");
 
-// Write password to the #password input
+// The code that was provided beforehand 
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
 }
-
+//Added a function to generate the password 
 function generatePassword () {
+  //Added console log to annotate/zone in on what is going on with the application 
+  console.log("Clicked the Button")
+  
+ 
+  // Added the requirements and made them into 4 different variables in a string of code for all the possible chractera that can be used in the password generator
  const uppercase= ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
  const lowercase = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"]
  const numbers= [1,2,3,4,5,6,7,8,9,0]
- const special= ["!@#$%^&*()-_,/?~;"]
+ const special= ["!@#$%&'()*+,-./:;<=>?[\]^_`{|}~"]
+ //Followed the *** guide to include all the special chracters that can be included within the generated password 
+
+//Initialize the variables 
+
+ //Prompt the user on how many characters they would want in the generated password by using an if statement  
+ pswLength =windowprompt ("How many chracters you would like your password to contain?")
+ if (pswlength < 8 || pswlength >128) {
+  window.alert ("Password needs to be between 8 and 128 chracters")
+  return;
+ }
+// Prompt user on what they want to encorporate within their passwords
+ passworduppercase = window.confirm("Click OK to confirm the use of Uppercase letters in password")
+ passwordlowercase = window.confirm("Click OK to confirm the use of Lowercase letters in password")
+ passwornumbers = window.confirm("Click OK to confirm the use of numbers in password")
+ passwordspecial = window.confirm("Click OK to confirm the use of special chracters in password")
+
+ 
+ // Validate the input 
+
+// Generate the password in the page 
+
+
+ //Added a return function 
+ return "Generated Password will go here";
+
 }
 
 
